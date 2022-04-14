021f30819d84f9ac45eb0a80c40db51345829e1d
Eslim Daga <esliim9811@gmail.com>
2022-04-14 10:43:11 -0500

    Agregue el layout como envolvente del tema

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./assets/styles/index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import Layout from "./template/Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Layout>
+          <App />
+        </Layout>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
