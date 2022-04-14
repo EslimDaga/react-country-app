5711493b001b227658acaeab574bae119b7747b5
Eslim Daga <esliim9811@gmail.com>
2022-04-14 13:00:29 -0500

    Elimine archivos que ya no uso para agregar el figma

--- src/views/Countries.jsx
+++ src/views/Countries.jsx
@@ -1,19 +1,24 @@
-import { SearchCircleIcon } from "@heroicons/react/solid";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Countries = () => {
+
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get("https://restcountries.com/v3.1/all")
+      .then(({ data }) => {
+        setCountries(data);
+        setLoading(false);
+      });
+  },[]);
+
   return (
-    <div className="w-full block sm:block md:flex lg:flex justify-between px-6 sm:px-6 md:px-6 lg:px-20 my-8">
-      <div className="relative">
-        <input
-          type="text"
-          className="w-full sm:w-64 md:w-64 lg:w-72 h-14 bg-white dark:bg-gray-900 placeholder:text-gray-600 text-gray-700 dark:text-white pr-8 pl-11 rounded-lg z-0 focus:shadow focus:outline-yellow-400 dark:focus:shadow dark:focus:outline-none font-fredoka font-normal"
-          placeholder="Buscar"
-        />
-        <div className="absolute top-4 left-3">
-          <SearchCircleIcon className="w-6 h-6 text-gray-600" />
-        </div>
-      </div>
-    </div>
+    <section className="flex-1 overflow-y-auto">
+
+    </section>
   );
 }
 
