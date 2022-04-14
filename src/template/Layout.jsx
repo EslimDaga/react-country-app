e4b4d7703787897bc26bda36b2b49e8eda157ea7
Eslim Daga <esliim9811@gmail.com>
2022-04-14 11:00:51 -0500

    Agregue este archivo como envolvente para todo el tema

---
+++ src/template/Layout.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/common/Header";
+
+const Layout = ({ children }) => {
+  return (
+  <div className="bg-gray-100 dark:bg-gray-900 h-screen flex flex-col">
+    <Header />
+    {children}
+  </div>);
+}
+
+export default Layout
