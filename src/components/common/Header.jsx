f1ebd77afa00fd1b09337d7fc2ca547519553b31
Eslim Daga <esliim9811@gmail.com>
2022-04-14 23:01:44 -0500

    Agregue un Link

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,5 +1,6 @@
 import { Popover } from "@headlessui/react";
 import Toggle from "./ToggleMode";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -7,11 +8,11 @@ const Header = () => {
       <div className="mx-auto px-6 sm:px-6 md:px-6 lg:px-20">
         <div className="flex justify-between items-center py-6 md:justify-between md:space-x-10">
           <div className="flex justify-between lg:w-0 lg:flex-1">
-            <a href="/">
+            <Link to="/">
               <h1 className="font-Nunito-Sans font-bold text-base sm:text-base md:text-2xl lg:text-2xl text-gray-800 dark:text-white">
                 Where in the world?
               </h1>
-            </a>
+            </Link>
           </div>
           <Toggle />
         </div>
